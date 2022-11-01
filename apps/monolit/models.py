527a113713884b152b89e64cac5e2052b2459eea
TimSkoro <Tsimafei.Skarakhodau@itechart-group.com>
2022-11-01 22:43:53 +0300

    feat: created site endpoints

--- apps/monolit/models.py
+++ apps/monolit/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from colorfield.fields import ColorField
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, unique_for_date='date')
+    title_color = ColorField(default=0xffffff)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    images = models.ImageField(null=True)
+
+    def __str__(self):
+        return f'{self.title}-{self.date}'
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-&egpbtnsrjcx++z#*8$&$endqoq-@b5ca)$usb9lxf)+^m5quj
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
@@ -40,6 +38,7 @@ INSTALLED_APPS = [
 
     # Third party
     'rest_framework',
+    'colorfield',
 
     # Local
     'apps.api.apps.ApiConfig',
@@ -77,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -87,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -107,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -119,11 +115,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
